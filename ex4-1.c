a1a79f58e4af8fde9d291784671572a874540071
s1280233 <86814051+s1280233@users.noreply.github.com>
2021-07-02 15:10:33 +0900

    Final revision

--- ex4-1.c
+++ ex4-1.c
@@ -23,8 +23,8 @@ int main(){
 
   printf("Total value: %d\n", sum);
   
-  if(sum > 7) printf("You won\n");
-  else printf("You lost\n");
+  if(sum > 7) printf("%s won\n", str);
+  else printf("%s lost\n", str);
   
   return 0;
 }
