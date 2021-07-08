4e47bcb84ae0912c28d921e6e4c4cf0ce0e471c3
s1280233 <s1280233@u-aizu.ac.jp>
2021-07-08 10:35:14 +0900

    Greetings added

--- Dice Game.c
+++ Dice Game.c
@@ -3,6 +3,14 @@
 #include <time.h>
 int main(){
   int i, sum=0, value;
+
+  //　名前を聞くパート
+  char str[8];
+  printf("What is your name?\n");
+  scanf("%s", str);
+  printf("Hello, %s!\n", str);
+
+  //　サイコロを振るパート
   printf("Rolling the dice...\n");
   srand((unsigned int)time(NULL));
   for(i=1;i<3;i++){
